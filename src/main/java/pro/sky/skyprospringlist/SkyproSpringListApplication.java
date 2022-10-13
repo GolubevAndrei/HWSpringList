315c0a3717730c8ccd42f625caecc9da4b54b7cc
GolubevAndrey <golubev_andrei87@mail.ru>
2022-10-13 14:14:57 +0300

    Добавлен Employee

--- src/main/java/pro/sky/skyprospringlist/SkyproSpringListApplication.java
+++ src/main/java/pro/sky/skyprospringlist/SkyproSpringListApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkyproSpringListApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SkyproSpringListApplication.class, args);
 	}
