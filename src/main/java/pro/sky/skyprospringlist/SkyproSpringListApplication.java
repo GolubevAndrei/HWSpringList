7877b703a8b24b5f03380dcca297fad60af9f69f
GolubevAndrey <golubev_andrei87@mail.ru>
2022-10-13 13:48:10 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringlist/SkyproSpringListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringlist/SkyproSpringListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
